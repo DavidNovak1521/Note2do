aba5e4a87856d3b9a750b238f4edaa60ba154b77
Dávid Novák <david.novak@appsolution.hu>
2022-11-16 20:39:45 +0100

    Colors added

---
+++ lib/style/my_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  // Color(0xFF...)
+  static Color get primaryBlue => const Color(0xFF013475); // #013475
+  static Color get secondaryBlue => const Color(0xFF6078E4); // #6078E4
+  static Color get tertiaryBlue => const Color(0xFF1FF3FD); // #1FF3FD
+  static Color get dark => const Color(0xFF151B2B); // #151B2B
+  static Color get white => const Color(0xFFFFFFFF); // #FFFFFF
+  static Color get black => const Color(0xFF000000); // #000000
+}
