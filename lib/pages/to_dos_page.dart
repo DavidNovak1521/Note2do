3adef402079a7b01e5de22be58603b247f3efde1
Dávid Novák <david.novak@appsolution.hu>
2022-11-20 13:27:14 +0100

    ToDos list, new ToDoItem widget

--- lib/pages/to_dos_page.dart
+++ lib/pages/to_dos_page.dart
@@ -2,9 +2,53 @@ import 'package:flutter/material.dart';
 
 import '../widgets/basic_page_container.dart';
 import '../widgets/my_title_text.dart';
+import '../widgets/items/to_do_item.dart';
 
 class ToDosPage extends StatelessWidget {
-  const ToDosPage({super.key});
+  final List<Map<String, dynamic>> tempToDos = [
+    {
+      'text': 'Első teendő',
+      'isDone': true,
+    },
+    {
+      'text': 'Második, de már hosszabb teendő',
+      'isDone': true,
+    },
+    {
+      'text': 'Harmadik, de még az előzőnél is hosszabb teendő tesztelni',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 4',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 5',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 6',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 7',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 8',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 9',
+      'isDone': false,
+    },
+    {
+      'text': 'Valami szöveg 10',
+      'isDone': false,
+    },
+  ];
+
+  ToDosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +57,18 @@ class ToDosPage extends StatelessWidget {
         slivers: [
           const SliverToBoxAdapter(
             child: MyTitleText(text: 'To Do\'s'),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 10,
+              (context, index) => Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                child: ToDoItem(
+                  text: tempToDos[index]['text'],
+                  isDone: tempToDos[index]['isDone'],
+                ),
+              ),
+            ),
           ),
         ],
       ),
---
+++ lib/widgets/items/to_do_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../style/my_colors.dart';
+
+class ToDoItem extends StatefulWidget {
+  final String text;
+  bool isDone;
+
+  ToDoItem({
+    super.key,
+    required this.text,
+    required this.isDone,
+  });
+
+  @override
+  State<ToDoItem> createState() => _ToDoItemState();
+}
+
+class _ToDoItemState extends State<ToDoItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: MyColors.secondaryBlue,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: ListTile(
+        onTap: () {
+          setState(() {
+            widget.isDone = !widget.isDone;
+          });
+        },
+        contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+        title: Text(
+          widget.text,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: GoogleFonts.montserrat(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: widget.isDone
+                ? MyColors.white.withOpacity(0.5)
+                : MyColors.white,
+            decoration: widget.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        leading: Icon(
+          widget.isDone
+              ? Icons.check_box_rounded
+              : Icons.check_box_outline_blank_rounded,
+          color: MyColors.white,
+        ),
+      ),
+    );
+  }
+}
