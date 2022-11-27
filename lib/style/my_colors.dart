b465248a85d1093675d2be4b83f73631b0590d87
Dávid Novák <david.novak@appsolution.hu>
2022-11-27 22:59:58 +0100

    Color hexa correction

--- lib/style/my_colors.dart
+++ lib/style/my_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyColors {
   // Color(0xFF...)
   static Color get primaryBlue => const Color(0xFF013475); // #013475
-  static Color get secondaryBlue => const Color(0xF51F93FD); // #1FF3FD
+  static Color get secondaryBlue => const Color(0xFF1F93FD); // #1F93FD
   static Color get tertiaryBlue => const Color(0xFF6078E4); // #6078E4
   static Color get buttonBlue => const Color(0xFF1FF3FD); // #1FF3FD
   static Color get dark => const Color(0xFF151B2B); // #151B2B
