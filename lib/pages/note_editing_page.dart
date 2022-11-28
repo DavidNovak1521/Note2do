838a263f4e3e5c395e7126d0a685cf7caba0d354
Zsotya <43848319+Zsotya@users.noreply.github.com>
2022-11-28 21:00:23 +0100

    NoteEditingPage two buttons added, copy to clipboard function working

--- lib/pages/note_editing_page.dart
+++ lib/pages/note_editing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../widgets/basic_page_container.dart';
@@ -31,38 +32,90 @@ class NoteEditingPage extends StatelessWidget {
     final textController = TextEditingController(text: note.text);
 
     return BasicPageContainer(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextField(
-              controller: titleController,
-              style: titleTextStyle,
-              cursorColor: MyColors.dark,
-              textAlign: TextAlign.center,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Write a title ...',
+      needsMask: false,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 80),
+            child: SingleChildScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: titleController,
+                    style: titleTextStyle,
+                    cursorColor: MyColors.dark,
+                    textAlign: TextAlign.center,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Write a title ...',
+                    ),
+                  ),
+                  TextField(
+                    controller: textController,
+                    maxLines: null,
+                    style: textTextStyle,
+                    cursorColor: MyColors.dark,
+                    textAlign: TextAlign.start,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Write a new note ...',
+                    ),
+                  ),
+                ],
               ),
             ),
-            TextField(
-              controller: textController,
-              maxLines: null,
-              style: textTextStyle,
-              cursorColor: MyColors.dark,
-              textAlign: TextAlign.start,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Write a new note ...',
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(10),
+                      backgroundColor: MyColors.secondaryBlue,
+                      foregroundColor: MyColors.white,
+                    ),
+                    onPressed: () {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                      Clipboard.setData(
+                        ClipboardData(
+                          text:
+                              '${titleController.text}\n\n${textController.text}',
+                        ),
+                      );
+                    },
+                    child: const Icon(
+                      Icons.copy_rounded,
+                      size: 40,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(10),
+                      backgroundColor: MyColors.primaryBlue,
+                      foregroundColor: MyColors.white,
+                    ),
+                    onPressed: () {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                    child: const Icon(
+                      Icons.done_rounded,
+                      size: 40,
+                    ),
+                  ),
+                ],
               ),
             ),
-            const Divider(
-              height: 30,
-              color: Colors.transparent,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
